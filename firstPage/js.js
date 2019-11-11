7172e14fea5b8343f63d1c685d9429b00948b826
TimoBrandt1 <41124729+TimoBrandt1@users.noreply.github.com>
2019-11-11 11:27:26 +0100

    First Commit

---
+++ firstPage/js.js
@@ -0,0 +1,3 @@
+document.getElementById("myButton").onclick = function () {
+        location.href = "file:///C:/Users/Timo%20Brandt/Documents/ma/bewijzenmap/periode1.2/tpr/blok13/ShirtPage/1/index.html";
+    };
